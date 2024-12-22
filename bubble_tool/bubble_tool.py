be4e244b993a79660476e16d97b7985a38d6fc41
s9roll7 <s9roll74@gmail.com>
2024-12-22 16:04:48 +0900

    Added comic panel extraction function Change lama model

--- bubble_tool/bubble_tool.py
+++ bubble_tool/bubble_tool.py
@@ -1,6 +1,8 @@
 import logging
 import re
 from pathlib import Path
+import os
+
 
 import cv2
 import numpy as np
@@ -32,6 +34,8 @@ BUBBLE_BORDER_WIDTH = 0             # 0 pixel -> auto-calculation
 
 #############################################################################
 
+
+
 def get_image_file_list(img_dir_path:Path):
     img_list = [p for p in img_dir_path.glob("*") if re.search(r'.*\.(jpg|png|webp)', str(p))]
     return sorted(img_list)
@@ -62,6 +66,48 @@ def prepare_yolo():
         hf_hub_download(
             repo_id="kitsumed/yolov8m_seg-speech-bubble", subfolder=PurePosixPath(path.parent), filename=PurePosixPath(path.name), local_dir="data/models/yolov8m_seg-speech-bubble"
         )
+
+def prepare_lama():
+    import os
+    from pathlib import PurePosixPath
+
+    from huggingface_hub import hf_hub_download
+
+    if os.environ.get("LAMA_MODEL", None) == None:
+        os.environ["LAMA_MODEL"] = "data/models/AnimeMangaInpainting/model.jit.pt"
+    else:
+        return
+
+    os.makedirs("data/models/AnimeMangaInpainting", exist_ok=True)
+    for hub_file in [
+        "model.jit.pt",
+    ]:
+        path = Path(hub_file)
+
+        saved_path = "data/models/AnimeMangaInpainting" / path
+
+        if os.path.exists(saved_path):
+            continue
+
+        hf_hub_download(
+            repo_id="s9roll74/tracing_dreMaz_AnimeMangaInpainting", subfolder=PurePosixPath(path.parent), filename=PurePosixPath(path.name), local_dir="data/models/AnimeMangaInpainting"
+        )
+    
+    if False:
+        from bubble_tool.inpainting_lama_mpe import LamaFourier
+        def load_lama_mpe(model_path, device, use_mpe: bool = True, large_arch: bool = False) -> LamaFourier:
+            model = LamaFourier(build_discriminator=False, use_mpe=use_mpe, large_arch=large_arch)
+            sd = torch.load(model_path, map_location = 'cpu')
+            model.generator.load_state_dict(sd['gen_state_dict'])
+            if use_mpe:
+                model.mpe.load_state_dict(sd['str_state_dict'])
+            model.eval().to(device)
+            return model
+
+        model = load_lama_mpe("data/models/AnimeMangaInpainting/lama_large_512px.ckpt",  device='cpu', use_mpe=False, large_arch=True)
+        traced_model = torch.jit.trace(model.generator, ( torch.zeros([1, 3, 512, 512]), torch.zeros([1,1,512, 512])))
+        traced_model.save("data/models/AnimeMangaInpainting/model.jit.pt")
+
 
 # https://github.com/ultralytics/ultralytics/issues/3560
 def scale_image_torch(masks, im0_shape, ratio_pad=None):
@@ -108,7 +154,9 @@ def scale_image_torch(masks, im0_shape, ratio_pad=None):
 
 
 
-def detect_bubble(src_list, mask_path:Path, model_type, detection_th):
+#############################################################################
+
+def detect_bubble(src_list, mask_path:Path, model_type, detection_th, classification):
 
     if model_type == 0:
         prepare_yolo()
@@ -122,7 +170,7 @@ def detect_bubble(src_list, mask_path:Path, model_type, detection_th):
 
     for i, src in tqdm( enumerate(src_list), desc=f"detect_bubble", total=len(src_list)):
 
-        result_path = mask_path / Path(str(i).zfill(8) + ".png")
+        result_path = mask_path / Path(Path(src).with_suffix(".png").name)
 
         org_size = Image.open(src).size
 
@@ -132,23 +180,41 @@ def detect_bubble(src_list, mask_path:Path, model_type, detection_th):
         masks = results[0].masks
         boxes = results[0].boxes
 
-        result = None
+        result = [None,None,None]
 
         if masks is not None:
             for mask, box in zip(masks,boxes):
-                #logger.info(f"{box.conf=}")
-                mask = scale_image_torch(mask.data, (org_size[1],org_size[0]))
-                if result is not None:
-                    result += mask.squeeze()
-                else:
-                    result = mask.squeeze()
-            
-            result = result.cpu().numpy()
-            result = result.astype('uint8') * 255
+                cls = int(box.cls)
+                if cls > 2:
+                    cls = 2
 
-            Image.fromarray(result).save(result_path)
+                mask = scale_image_torch(mask.data, (org_size[1],org_size[0]))
+                if result[cls] is not None:
+                    result[cls] += mask.squeeze()
+                else:
+                    result[cls] = mask.squeeze()
+            
+            for i in range(len(result)):
+                if result[i] is not None:
+                    result[i] = result[i].cpu().numpy()
+                    result[i] = result[i].astype('uint8') * 255
+                else:
+                    result[i] = np.zeros((org_size[1],org_size[0]), np.uint8)
+            
+            if classification:
+                result_array = np.dstack(result)
+            else:
+                result_array = result[0] | result[1] | result[2]
+
+            Image.fromarray(result_array).save(result_path)
         else:
-            Image.fromarray( np.zeros((org_size[1],org_size[0]), np.uint8) ).save(result_path)
+            if classification:
+                result_array = np.zeros((org_size[1],org_size[0],3), np.uint8)
+            else:
+                result_array = np.zeros((org_size[1],org_size[0]), np.uint8)
+
+            Image.fromarray( result_array ).save(result_path)
+            
 
         if False:
             bubble_array = np.array(Image.open(src))
@@ -164,11 +230,13 @@ def detect_bubble(src_list, mask_path:Path, model_type, detection_th):
 
 def lama_inpaint(src_list, mask_list, output_path:Path):
 
+    prepare_lama()
+
     simple_lama = SimpleLama()
 
     for i, (src, mask) in tqdm( enumerate(zip(src_list,mask_list)), desc=f"lama_inpaint", total=min(len(src_list),len(mask_list))):
 
-        result_path = output_path / Path(str(i).zfill(8) + ".png")
+        result_path = output_path / Path(Path(src).name)
 
         image = Image.open(src)
 
@@ -253,11 +321,32 @@ def blend_image_B(org_array, mask_array, dst_array):
 
     return dst_array
 
+def blend_image_C(org_array, mask_array, dst_array):
+
+    org_array[mask_array==0] = (0)
+
+    gray = cv2.cvtColor(org_array, cv2.COLOR_RGB2GRAY)
+    ret, binary = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+
+    contours, hierarchy = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    mask = np.zeros((org_array.shape[0],org_array.shape[1]), np.uint8)
+    mask = cv2.drawContours(mask, contours, -1, (255), -1)
+
+    if mask.ndim == 2:
+        mask = mask[:, :, np.newaxis]
+    
+    mask = mask / 255
+
+    dst_array = org_array * mask + dst_array * (1 - mask)
+
+    return dst_array
+
+
 def blend_image(src_list, mask_list, base_list, output_path:Path, blend_method):
 
     for i, (src, mask, base) in tqdm( enumerate(zip(src_list,mask_list,base_list)), desc=f"blend_image", total=min(len(src_list),len(mask_list),len(base_list))):
 
-        result_path = output_path / Path(str(i).zfill(8) + ".png")
+        result_path = output_path / Path(Path(src).name)
 
         org_array = np.array(Image.open(src))
         mask_array = np.array(Image.open(mask))
@@ -265,13 +354,28 @@ def blend_image(src_list, mask_list, base_list, output_path:Path, blend_method):
         org_array = org_array[:,:,:3]
         dst_array = dst_array[:,:,:3]
 
+        if blend_method != -1:
+            if mask_array.ndim == 3:
+                mask_array = mask_array[:, :, 0] | mask_array[:, :, 1] | mask_array[:, :, 2]
+
+        # DEBUG
+        #dst_array[:,:,:] = 125
+
         org_array = resize_img( org_array, (dst_array.shape[1], dst_array.shape[0]) )
         mask_array = resize_img( mask_array, (dst_array.shape[1], dst_array.shape[0]) )
 
-        if blend_method == 0:
+        if blend_method == -1:
+
+            dst_array = blend_image_A(org_array.copy(), mask_array[:, :, 0], dst_array)
+            dst_array = blend_image_B(org_array.copy(), mask_array[:, :, 1], dst_array)
+            dst_array = blend_image_B(org_array.copy(), mask_array[:, :, 2], dst_array)
+
+        elif blend_method == 0:
             dst_array = blend_image_A(org_array, mask_array, dst_array)
-        else:
+        elif blend_method == 1:
             dst_array = blend_image_B(org_array, mask_array, dst_array)
+        else:
+            dst_array = blend_image_C(org_array, mask_array, dst_array)
 
         Image.fromarray(dst_array.astype(np.uint8)).save(result_path)
 
@@ -280,7 +384,7 @@ def remove_bubble(src_path:Path, mask_output_path:Path, clean_img_output_path:Pa
 
     src_list = get_image_file_list(src_path)
 
-    detect_bubble(src_list, mask_output_path, model_type, detection_th)
+    detect_bubble(src_list, mask_output_path, model_type, detection_th, False)
 
     mask_list = get_image_file_list(mask_output_path)
 
@@ -290,13 +394,133 @@ def remove_bubble(src_path:Path, mask_output_path:Path, clean_img_output_path:Pa
 
 def copy_bubble(src_path:Path, base_path:Path, mask_input_path:Path, create_mask, with_bubble_img_output_path:Path, model_type, blend_method, detection_th):
 
+    base_list = get_image_file_list(base_path)
     src_list = get_image_file_list(src_path)
 
-    if create_mask:
-        detect_bubble(src_list, mask_input_path, model_type, detection_th)
+    src_map = { s.stem : s for s in src_list }
 
-    base_list = get_image_file_list(base_path)
-    mask_list = get_image_file_list(mask_input_path)
+    src_list = []
+
+    for b in base_list:
+        src_img_path = src_map.get(b.stem, None)
+
+        if src_img_path:
+            if src_img_path.is_file():
+                src_list.append( src_img_path )
+
+
+    if create_mask:
+        detect_bubble(src_list, mask_input_path, model_type, detection_th, True)
+
+    mask_list = [ (mask_input_path/Path(s.name)).with_suffix(".png") for s in base_list if (mask_input_path/Path(s.name)).with_suffix(".png").is_file() ]
 
     blend_image(src_list, mask_list, base_list, with_bubble_img_output_path, blend_method)
+
+
+
+def split_panel(src_path:Path, split_output_path:Path, output_size):
+    from transformers import AutoModel
+
+    src_list = get_image_file_list(src_path)
+
+    def read_image_as_np_array(image_path):
+        with open(image_path, "rb") as file:
+            image = Image.open(file).convert("L").convert("RGB")
+            image = np.array(image)
+        return image
+    
+    images = [read_image_as_np_array(image) for image in src_list]
+
+    model = AutoModel.from_pretrained("ragavsachdeva/magi", trust_remote_code=True).cuda()
+    with torch.no_grad():
+        results = model.predict_detections_and_associations(images)
+
+    crop_info = []
+
+    for i, (result, src_path) in enumerate(zip(results, src_list)) :
+        with Image.open(src_path) as im:
+            crop_info.append(f"{src_path.name},{im.size[0]},{im.size[1]}")
+            for j, panel in enumerate(result["panels"]):
+                im_crop = im.crop(panel)
+                scale = output_size / (im_crop.size[0] + im_crop.size[1])
+                im_crop = im_crop.resize( (int(im_crop.size[0] * scale), int(im_crop.size[1] * scale)), Image.LANCZOS )
+                filename = str(i).zfill(8) + "_" + str(j).zfill(8) + ".png"
+                im_crop.save( split_output_path / Path(filename) )
+                crop_info.append(f"{filename},{panel[0]},{panel[1]},{panel[2]},{panel[3]}")
+    
+    crop_info_path = split_output_path / Path("split_info.txt")
+    crop_info_path.write_text( "\n".join(crop_info), encoding="utf-8")
+
+    model.to("cpu")
+
+    torch.cuda.empty_cache()
+
+
+def combine_panel(src_path:Path, fragment_path:Path, output_dir:Path):
+
+    crop_info_path = fragment_path / Path("split_info.txt")
+    if crop_info_path.is_file() == False:
+        raise ValueError(f"{crop_info_path} not found")
+    
+    crop_list = crop_info_path.read_text()
+    crop_list = crop_list.splitlines()
+
+    def create_crop_info(crop_list):
+        crop_info = {}
+        cur_src_name = None
+        cur_item = {}
+
+        for c in crop_list:
+            c = c.split(",")
+            if len(c) == 3:
+                if cur_item:
+                    crop_info[cur_src_name] = cur_item
+                cur_item = {}
+                cur_src_name, x, y = c
+                cur_item["org_size"] = (int(x), int(y))
+                cur_item["frags"] = {}
+            else:
+                frag_name, x1, y1, x2, y2 = c
+                x1 = int(float(x1))
+                y1 = int(float(y1))
+                x2 = int(float(x2))
+                y2 = int(float(y2))
+                cur_item["frags"][frag_name] = (x1,y1,x2,y2)
+
+        if cur_item:
+            crop_info[cur_src_name] = cur_item
+        return crop_info
+    
+
+    crop_info = create_crop_info(crop_list)
+    
+
+    for i, src_name in enumerate(crop_info):
+        src_img_path = src_path / Path(src_name)
+        if src_img_path.is_file() == False:
+            continue
+        src_img = Image.open( src_img_path )
+        org_size = crop_info[src_name]["org_size"]
+
+        mod = False
+
+        for frag_name in crop_info[src_name]["frags"]:
+            frag_img_path = fragment_path / Path(frag_name)
+            if frag_img_path.is_file() == False:
+                frag_img_path = frag_img_path.with_suffix(".png")
+                if frag_img_path.is_file() == False:
+                    continue
+            frag_img = Image.open(frag_img_path)
+            x1,y1,x2,y2 = crop_info[src_name]["frags"][frag_name]
+
+            scale_x = (x2-x1) / org_size[0]
+            scale_y = (y2-y1) / org_size[1]
+            frag_img = frag_img.resize( (int(src_img.size[0] * scale_x), int(src_img.size[1] * scale_y)), Image.LANCZOS )
+            scale_x = src_img.size[0]/org_size[0]
+            scale_y = src_img.size[1]/org_size[1]
+            src_img.paste(frag_img, (int(x1 * scale_x),int(y1 * scale_y)) )
+            mod = True
+
+        if mod:
+            src_img.save( output_dir/ Path(src_name) )
 
--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import time
 from pathlib import Path
 import fire
 from datetime import datetime
+import shutil
 
-from bubble_tool.bubble_tool import remove_bubble,copy_bubble
+from bubble_tool.bubble_tool import remove_bubble, copy_bubble, split_panel, combine_panel
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -25,9 +26,15 @@ def create_proj_dirs(proj_dir_path:Path):
     (proj_dir_path / Path("base_with_bubble")).mkdir(parents=True, exist_ok=True)
 
 
+
 class Command:
     def __init__(self):
-        pass
+        import os
+        if os.name == 'nt':
+            import _locale
+            if not hasattr(_locale, '_gdl_bak'):
+                _locale._gdl_bak = _locale._getdefaultlocale
+                _locale._getdefaultlocale = (lambda *args: (_locale._gdl_bak()[0], 'UTF-8'))
 
     def remove(self, src_path, model_type:int=0, detection_th:float=0.1):
         # src image -> mask + clean image
@@ -50,7 +57,7 @@ class Command:
         logger.info(f"Total Elapsed time : {time.time() - start_tim}")
 
 
-    def copy(self, src_path, base_path, mask_path=None, model_type:int=0, blend_method:int=0, detection_th:float=0.1):
+    def copy(self, src_path, base_path, mask_path=None, model_type:int=0, blend_method:int=-1, detection_th:float=0.1):
         # src image, base_image ,(bubble_mask) -> base_with_bubble image
         start_tim = time.time()
 
@@ -86,7 +93,45 @@ class Command:
         logger.info(f"Total Elapsed time : {time.time() - start_tim}")
 
 
-    def remove_proj(self, proj_dir_path, model_type:int=0, detection_th:float=0.1):
+    def split(self, src_path, frag_size=1024*2):
+        # src image -> split images
+        start_tim = time.time()
+
+        src_path = Path(src_path)
+        if not src_path.is_dir():
+            raise ValueError( f"{src_path} not found" )        
+
+        output_dir = Path("output") / Path("split") / Path(get_time_str())
+        output_dir.mkdir(parents=True)
+
+        split_panel(src_path, Path(output_dir), frag_size)
+
+        logger.info(f"Output : {output_dir}")
+        logger.info(f"Total Elapsed time : {time.time() - start_tim}")
+
+
+    def combine(self, src_path, frag_path):
+        # src image + frag images -> combined image
+        start_tim = time.time()
+
+        src_path = Path(src_path)
+        if not src_path.is_dir():
+            raise ValueError( f"{src_path} not found" )        
+
+        frag_path = Path(frag_path)
+        if not frag_path.is_dir():
+            raise ValueError( f"{frag_path} not found" )
+        
+        output_dir = Path("output") / Path("combine") / Path(get_time_str())
+        output_dir.mkdir(parents=True)
+
+        combine_panel(src_path, frag_path, Path(output_dir))
+
+        logger.info(f"Output : {output_dir}")
+        logger.info(f"Total Elapsed time : {time.time() - start_tim}")
+
+
+    def remove_proj(self, proj_dir_path, model_type:int=0, detection_th:float=0.1, split=True, frag_size=1024*2):
         # src image -> mask + clean image
         start_tim = time.time()
 
@@ -104,16 +149,33 @@ class Command:
         
         mask_output_path = proj_dir_path / Path("mask") / Path(time_str)
         mask_output_path.mkdir(parents=True)
-        clean_img_output_path = proj_dir_path / Path("cleaned") / Path(time_str)
-        clean_img_output_path.mkdir(parents=True)
 
-        remove_bubble(src_path, mask_output_path, clean_img_output_path, model_type, detection_th)
+        if split == False:
+            clean_img_output_path = proj_dir_path / Path("cleaned") / Path(time_str)
+            clean_img_output_path.mkdir(parents=True)
+            remove_bubble(src_path, mask_output_path, clean_img_output_path, model_type, detection_th)
 
-        logger.info(f"Output : {clean_img_output_path}")
+            logger.info(f"Output : {clean_img_output_path}")
+        else:
+            clean_img_output_path = proj_dir_path / Path("pre_split") / Path(time_str)
+            clean_img_output_path.mkdir(parents=True)
+            split_output_path = proj_dir_path / Path("cleaned") / Path(time_str)
+            split_output_path.mkdir(parents=True)
+            remove_bubble(src_path, mask_output_path, clean_img_output_path, model_type, detection_th)
+
+            split_panel(clean_img_output_path, split_output_path, frag_size)
+
+            split_info_path = split_output_path / Path("split_info.txt")
+            split_info_path2 = proj_dir_path / Path("base") / Path("split_info.txt")
+
+            shutil.copy(split_info_path, split_info_path2)
+
+            logger.info(f"Output : {split_output_path}")
+
         logger.info(f"Total Elapsed time : {time.time() - start_tim}")
 
 
-    def copy_proj(self, proj_dir_path, model_type:int=0, blend_method:int=0, detection_th:float=0.1):
+    def copy_proj(self, proj_dir_path, model_type:int=0, blend_method:int=-1, detection_th:float=0.1):
         # src image, base_image ,(bubble_mask) -> base_with_bubble image
         start_tim = time.time()
 
@@ -138,7 +200,17 @@ class Command:
         with_bubble_img_output_path = proj_dir_path / Path("base_with_bubble") / Path(time_str)
         with_bubble_img_output_path.mkdir(parents=True)
 
-        copy_bubble(src_path, base_path, mask_output_path, True, with_bubble_img_output_path, model_type, blend_method, detection_th)
+        combine = (base_path / Path("split_info.txt")).is_file()
+
+        if combine == False:
+            copy_bubble(src_path, base_path, mask_output_path, True, with_bubble_img_output_path, model_type, blend_method, detection_th)
+        else:
+            combine_output_path = proj_dir_path / Path("combined") / Path(time_str)
+            combine_output_path.mkdir(parents=True)
+
+            combine_panel(src_path, base_path, combine_output_path)
+            copy_bubble(src_path, combine_output_path, mask_output_path, True, with_bubble_img_output_path, model_type, blend_method, detection_th)
+
 
         logger.info(f"Output : {with_bubble_img_output_path}")
         logger.info(f"Total Elapsed time : {time.time() - start_tim}")
